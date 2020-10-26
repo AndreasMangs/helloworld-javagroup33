9a6c454c7523cbb74a7f4f04fa72ebdd2a4cd761
AndreasMangs <andreas_mangs@hotmail.com>
2020-10-26 11:46:12 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/andreasm/App.java
@@ -0,0 +1,13 @@
+package se.lexicon.andreasm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
