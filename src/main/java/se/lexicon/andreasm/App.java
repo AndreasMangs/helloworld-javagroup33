b866b6c4f466ce80660c16d92be08bd5be645196
AndreasMangs <andreas_mangs@hotmail.com>
2020-10-27 14:03:22 +0100

    Added more

--- src/main/java/se/lexicon/andreasm/App.java
+++ src/main/java/se/lexicon/andreasm/App.java
@@ -8,8 +8,20 @@ public class App
 {
     public static void main( String[] args )
     {
+        int nummer=1;
         System.out.println( "Hello World!" );
         System.out.println( "Goodbye World!" );
         System.out.println( "No World!" );
+        // System.in.read(nummer);
+
+        int siffra=2;
+        int summa=nummer+siffra;
+        System.out.println(summa++);
+
+        String name = "Andreas";
+        name = name + " Mångs";
+        System.out.println(name);
+
+
     }
 }
